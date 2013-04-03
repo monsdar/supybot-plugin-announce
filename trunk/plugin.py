6068a399a17a4d93786c367b10e91de9b07238c7
monsdar <monsdar@dfde17ac-059f-1df9-6ad0-fd7a81750fcc>
2013-04-03 18:55:40 +0000

    Initial checkin

---
+++ trunk/plugin.py
@@ -0,0 +1,24 @@
+###
+# Copyright (c) 2013, Nils Brinkmann
+# All rights reserved.
+#
+#
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class Announce(callbacks.Plugin):
+    """Add the help for "@plugin help Announce" here
+    This should describe *how* to use this plugin."""
+    pass
+
+
+Class = Announce
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
